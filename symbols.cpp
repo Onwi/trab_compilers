a76244ce0bebc157fde8c1c6d618523b6b22ab42
Luis Coelho <luiscoelhors@gmail.com>
2025-03-28 01:29:44 -0300

    etapa1 with no refactoring

---
+++ symbols.cpp
@@ -0,0 +1,28 @@
+#include <cstdio>
+#include <cstdlib>
+#include <map>
+#include <ostream>
+#include <iostream>
+#include "symbols.hpp"
+
+std::map<std::string, Symbol*> symbolTable;
+
+Symbol *insert(int type, std::string text) {
+  std::map<std::string, Symbol*>::iterator it = symbolTable.find(text);
+  if (it != symbolTable.end())  {
+    return it->second;
+  }
+
+  Symbol *newSymbol = (Symbol *) malloc(sizeof(Symbol)); 
+  newSymbol->type = type;
+  newSymbol->text = text;
+  symbolTable[text] = newSymbol;
+
+  return newSymbol;
+}
+
+void printSymbolsTable() {
+  for (auto s : symbolTable) {
+    std::cout << "Symbol[" << s.second->type << "," << s.second->text << "]" << std::endl;
+  }
+}
---
+++ symbols.hpp
@@ -0,0 +1,9 @@
+#include <string>
+
+typedef struct SYMBOL {
+  int type;
+  std::string text;
+} Symbol;
+
+Symbol *insert(int type, std::string text);
+void printSymbolsTable();
